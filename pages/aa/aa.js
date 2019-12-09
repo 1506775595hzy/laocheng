833801bcf9f4d85db5f68e8d3eec4eb9b7e19b3e
1506775595hzy <56444398+1506775595hzy@users.noreply.github.com>
2019-12-09 09:38:14 +0800

    Add files via upload

---
+++ pages/aa/aa.js
@@ -0,0 +1,50 @@
+Page({
+
+data:{
+  
+},
+
+  jump1: function () {
+    wx.navigateTo({
+      url: '../page1/page1',
+      success: function (res) {
+
+      },
+      fail: function (res) {
+
+      },
+      complete: function (res) {
+
+      },
+    })
+  },
+  jump2: function () {
+    wx.navigateTo({
+      url: '../page2/page2',
+      success: function (res) {
+
+      },
+      fail: function (res) {
+
+      },
+      complete: function (res) {
+
+      },
+    })
+  },
+  jump3: function () {
+    wx.navigateTo({
+      url: '../index2/index2',
+      success: function (res) {
+
+      },
+      fail: function (res) {
+
+      },
+      complete: function (res) {
+
+      },
+    })
+  },
+
+})
